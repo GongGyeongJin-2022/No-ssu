b109601bdc361a20a25070d87890f51bb93b23b2
PRESSO <caff1nepill@gmail.com>
2022-07-10 14:24:07 +0900

    add google map

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-import Config from 'react-native-config';
+import React from "react";
+import { View, Text } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 const App = () => {
     return (
-        <View>
-            <Text>
-                {Config.GOOGLE_MAPS_ANDROID_API_KEY}
-            </Text>
+        <View style={{ flex: 1 }}>
+            <MapView
+                style={{ flex: 1 }}
+                provider={PROVIDER_GOOGLE}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+                isReady/>
         </View>
-
-    )
+    );
 }
 
 export default App;
