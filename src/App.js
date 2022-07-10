c9cf903b8fa340ee5de61eb6158892f0b881c17e
PRESSO <caff1nepill@gmail.com>
2022-07-10 14:20:19 +0900

    add react-native-config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import Config from 'react-native-config';
 
 const App = () => {
     return (
         <View>
             <Text>
-                Hello
+                {Config.GOOGLE_MAPS_ANDROID_API_KEY}
             </Text>
         </View>
 
