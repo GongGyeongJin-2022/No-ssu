1e26fe79e6ecfa57c68a919f1cc5b669720a9adb
PRESSO <caff1nepill@gmail.com>
2022-07-28 20:39:43 +0900

    add bottom sheet

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 import React from 'react';
-import { SafeAreaView } from "react-native";
-import 'react-native-gesture-handler';
 
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
 import Main from '@screens/Main';
 import MyPage from "@screens/MyPage";
 import Upload from "@screens/Upload";
 import Login from '~/Login';
 
-const Stack = createStackNavigator();
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 const App = () => {
     return (
-        <>
-            {/*<Login />*/}
-
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName="Main">
-                    <Stack.Screen
-                        name="Main"
-                        component={Main}
-                        options={{headerShown: false}}
-                    />
-                    <Stack.Screen
-                        name="MyPage"
-                        component={MyPage}
-                        options={{headerShown: false}}
-                    />
-                    <Stack.Screen
-                        name="Upload"
-                        component={Upload}
-                        options={{headerShown: false}}
-                    />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </>
+        <GestureHandlerRootView style={{flex: 1}}>
+            <BottomSheetModalProvider>
+                <Main />
+            </BottomSheetModalProvider>
+        </GestureHandlerRootView>
     )
 }
 
