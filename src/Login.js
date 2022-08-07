eda470859220628c6be88df722292e988d5bc3d7
SeInLee26 <108651556+SeInLee26@users.noreply.github.com>
2022-08-07 22:33:11 +0900

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import { Image, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import user_interface from '@assets/img/user_interface.png'
+import { Image, View, Text, TouchableOpacity, StyleSheet, Button } from 'react-native';
+import Logo from './assets/No_ssu_logo.png';
+import Kakao from './assets/chat.png';
 
-const Login = () => {
+const App = () => {
     return (
         <View style={styles.container}>
-            <Image
-                style={styles.image}
-                source={user_interface}/>
+        <Image source={Logo} style={styles.Image_Logo}
+        />
             <Text style={styles.Title}>
-                Login
-            </Text>
-            <Text style={styles.txt}>
-                Start With Your Google Or Kakao Account
+                실시간 쓰레기 위치 가이드
             </Text>
             <View>
                 <TouchableOpacity style= {styles.button1}>
-                    <Text style = {styles.button1_txt}>Login in Google</Text>
+                <View>
+                <Text style = {styles.button1_txt}>G    구글 계정으로 로그인</Text> 
+                </View>
                 </TouchableOpacity>
-                <Text>
-
-                </Text>
                 <TouchableOpacity style={styles.button2}>
-                    <Text style={styles.button2_txt}>Login in Kakao</Text>
+                 <Image source={Kakao}></Image>
+                    <Text style={styles.button2_txt}>카카오 계정으로 로그인</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.button3}>
-                    <Text style={styles.button3_txt}>로그인 없이 이용하기</Text>
+                    <Text style>로그인 없이 시작하기</Text>
                 </TouchableOpacity>
             </View>
         </View>
@@ -35,75 +32,72 @@ const Login = () => {
 const styles = StyleSheet.create({
     container :  {
         alignSelf : 'center',
+        top : 150
     },
+
+    Image_Logo : {
+        top : 20,
+        width : 200,
+        height : 200,
+        alignSelf : 'center'
+    },
+
+    Image_kakao : {
+        alignSelf : 'auto',
+        width : 100,
+        height : 100
+    },
+
     button1 : {
-        width : 264,
-        height : 50,
-        top : 230,
-        borderRadius : 30,
-        backgroundColor : '#93CE92',
-        alignSelf : 'center',
-        borderColor : '#000000',
-        borderWidth : 4
+        width : 300,
+        height : 45,
+        top : 120,
+        borderRadius : 5,
+        backgroundColor : '#EA4335',
+        alignSelf : 'center'
     },
     button1_txt : {
+        top : 9,
         alignSelf : 'center',
-        fontSize : 17,
-        color : '#000000',
+        fontSize : 16,
+        color : '#FFFFFF',
         fontWeight : "bold"
     },
     button2 : {
-        width : 264,
-        height : 50,
-        top : 250,
-        Color : '#FFFFFF',
-        borderRadius : 30,
-        backgroundColor : '#93CE92',
-        alignSelf : 'center',
-        borderColor : '#000000',
-        borderWidth : 4
+        width : 300,
+        height : 45,
+        top : 130,
+        borderRadius : 5,
+        backgroundColor : '#FAE100',
+        alignSelf : 'center'
     },
     button2_txt : {
-        alignSelf : 'center',
-        fontSize : 17,
+        top : 5,
+        fontSize : 16,
         color : '#000000',
-        fontWeight : "bold"
+        fontWeight : "bold", 
+        alignSelf : 'center'
     },
     Title : {
-        top : 140,
+        top : 45,
         alignSelf : 'center',
-        fontSize : 30,
+        fontSize : 19,
         color : '#000000',
         fontWeight : "bold"
     },
-    txt : {
-        alignSelf : 'center',
-        fontSize : 15,
-        top : 153
-    },
-    image : {
-        top : 130,
-        alignSelf : 'center',
-        width : 100,
-        height :100
-    },
     button3 : {
-        width : 150,
-        height : 30,
-        top : 300,
-        backgroundColor : '#6EBFB0',
-        Color : '#000000',
-        borderRadius : 30,
         alignSelf : 'center',
-        borderWidth : 2,
-        borderColor : '#000000'
+        width : 130,
+        height : 30,
+        top : 150,
+        Color : '#FFFFFF',
+        alignSelf : 'center'
     },
     button3_txt : {
-        alignSelf : 'center',
         color : '#ffffff',
-        fontSize : 12,
+        fontSize : 11,
         fontWeight : 'bold'
     }
 })
 
-export default Login;
+export default App;
