59d008e518884a2b51ec167f16f7efae531b087d
euichan2 <euichan1108@gmail.com>
2022-08-29 20:08:34 +0900

    add use api hook

--- src/hooks/Hooks.js
+++ src/hooks/Hooks.js
@@ -1,4 +1,6 @@
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
+import {useRecoilCallback} from "recoil";
+import {tokenState} from "@apis/atoms";
 
 export const useInterval = (callback, delay) => {
     const savedCallback = useRef();
@@ -16,4 +18,31 @@ export const useInterval = (callback, delay) => {
             return () => clearInterval(id);
         }
     }, [delay]);
+}
+
+export const useApi = (api, authHeader=false) => {
+    const [loading, setLoading] = useState(true);
+    const [resolved, setResolved] = useState();
+
+    const makeHeaders = (token) => {
+        return {
+            'Authorization': 'Bearer ' + token,
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const callback = useRecoilCallback(({snapshot, set}) =>
+            async (...args) => {
+                let access_token;
+                if(authHeader) {
+                    access_token = (await snapshot.getPromise(tokenState)).accessToken;
+                }
+                const {data} = await api(authHeader ? makeHeaders(access_token) : null, ...args);
+                setLoading(false);
+                setResolved(data);
+                return data
+            },
+        [],
+    );
+    return [loading, resolved, callback];
 }
