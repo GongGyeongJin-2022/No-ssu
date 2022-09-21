b5bde63e1f597d4168c12d5e4fe012f8e9cc7164
PRESSO <caff1nepill@gmail.com>
2022-09-21 22:38:55 +0900

    change api protocol

--- src/apis/apiServices.ts
+++ src/apis/apiServices.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosResponse} from "axios";
 import {Token} from "@apis/atoms";
 
-const PROTOCOL = "http://";
+const PROTOCOL = "https://";
 const AND_DEV_URL = PROTOCOL + "nossu.space";
 const IOS_DEV_URL = PROTOCOL +  "127.0.0.1:8000";
 export const URL = AND_DEV_URL;
